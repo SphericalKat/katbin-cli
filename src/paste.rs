5b483d6b58931b6555aa4aac5e59390db3f4a88e
Amogh Lele <amolele@gmail.com>
2022-05-07 20:37:24 +0530

    refactor(treewide): make pastes just a single command away
    Signed-off-by: Amogh Lele <amolele@gmail.com>

--- src/paste.rs
+++ src/paste.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use clap::{Args, Subcommand};
 use reqwest::blocking::Client;
 use serde::Deserialize;
 use serde_json::json;
@@ -9,23 +8,6 @@ pub struct PasteResponse {
     pub content: String,
     pub id: String,
     pub is_url: bool,
-}
-
-#[derive(Debug, Args)]
-#[clap(args_conflicts_with_subcommands = true)]
-pub struct Paste {
-    #[clap(subcommand)]
-    pub command: Option<PasteCommands>,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum PasteCommands {
-    Create(PasteCreate),
-}
-
-#[derive(Debug, Args)]
-pub struct PasteCreate {
-    pub body: String,
 }
 
 pub fn create_paste(body: String) -> Result<PasteResponse> {
